3989e9a1d9c92512a09f7c41453e2b6296f16a41
DerEine117 <robert.knabe@web.de>
2025-03-01 01:53:03 +0100

    added http error messages

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MDB_DATA_API_KEY = os.getenv("MDB_DATA_API_KEY", "doyi6ohchieKaeL9")
-MDB_DATA_URL = os.getenv("MDB_DATA_URL", "http://localhost:8001/api/v1/byName")
+MDB_DATA_URL = os.getenv("MDB_DATA_URL", "http://mdb-data:8001/api/v1/byName")
 
 PORT = int(os.getenv("PORT", 8002))
--- app/endpoints.py
+++ app/endpoints.py
@@ -10,22 +10,26 @@ def getByName(name: str):
         "apikey": MDB_DATA_API_KEY,
         "name": name
     }
-    response = requests.get(MDB_DATA_URL, params=params)
+    try:
+        response = requests.get(MDB_DATA_URL, params=params, timeout=2)
 
-    if response.status_code != 200:
-        raise HTTPException(status_code=response.status_code, detail="Fehler beim Abrufen der Daten")
-    else:
-        data = response.json()
-        if "data" not in data:
-            raise HTTPException(status_code=500, detail="Invalid response from MDB-Data")
+        if response.status_code == 401:
+            raise HTTPException(status_code=response.status_code, detail="ERROR: Invalid credentials")
+        elif response.status_code != 200:
+            raise HTTPException(status_code=500, detail="Error: INTERNAL SERVER ERROR")
+        else:
+            data = response.json()
+            if "data" not in data:
+                raise HTTPException(status_code=500, detail="ERROR: INTERNAL SERVER ERROR")
 
-        if data.get("records", 0) == 0:
-            raise HTTPException(status_code=204)
+            if data.get("records", 0) == 0:
+                raise HTTPException(status_code=204)
 
-        filtered_data = [{"id": record["id"], "titel": record["titel"]} for record in data["data"]]
+            filtered_data = [{"id": record["id"], "titel": record["titel"]} for record in data["data"]]
 
-        return filtered_data
-        #return response.json()
+            return filtered_data
+    except requests.exceptions.Timeout:
+        raise HTTPException(status_code=504, detail="ERROR: Timeout")
     
 @router.get("/health")
 def getHealth():
